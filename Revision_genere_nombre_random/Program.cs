af7b1d135668907017bf6a3d0d1e4b048b4df52d
Votre Nom <freezix.dev@gmail.com>
2023-12-17 23:29:54 +0100

    Premier commit

---
+++ Revision_genere_nombre_random/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+
+namespace Revision_genere_nombre_random
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //fonction 
+            int nombre = 0;
+
+            
+
+            Console.WriteLine("Combien de table voulez vous genere ?");
+            int nbTable = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nbTable; i++)
+            {
+                //generateur
+                Random rndm = new Random();
+                nombre = rndm.Next(12);
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("Voici une liste de " + nbTable + " Tables");
+                Console.Write(nombre);
+                Console.Write(" X " );
+                Console.Write(nombre);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("merci d'avoir utiliser Generateur de table !!!");
+
+
+        }
+
+        
+    }
+}
